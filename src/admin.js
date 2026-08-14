1ac68687a82bbf8473f9719f7103b8613366a742
appIeonlineport <indiatryme@gmail.com>
2026-08-15 05:03:08 +0530

    Update admin.js

--- src/admin.js
+++ src/admin.js
@@ -1,3 +1,5 @@
+import "./styles.css";
+import { createClient } from "@supabase/supabase-js";
 import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
