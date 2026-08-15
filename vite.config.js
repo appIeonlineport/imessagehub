d000d59745ba881438346dad778ebccd768210f1
appIeonlineport <indiatryme@gmail.com>
2026-08-15 07:29:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { resolve } from "path";
+import { fileURLToPath } from "url";
+import { defineConfig } from "vite";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = resolve(__filename, "..");
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        dashboard: resolve(__dirname, 'dashboard.html'),
-        admin: resolve(__dirname, 'admin.html'),
+        main: resolve(__dirname, "index.html"),
+        dashboard: resolve(__dirname, "dashboard.html"),
+        admin: resolve(__dirname, "admin.html"),
       },
     },
   },
